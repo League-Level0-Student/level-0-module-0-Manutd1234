ab483a4caf4db95aedca05f70843cb585a9f254e
league <burgerbiter2018@gmail.com>
2022-09-24 16:29:57 -0700

    Quiz

--- src/_03_print_and_popups/_4_quiz_game/QuizGame.java
+++ src/_03_print_and_popups/_4_quiz_game/QuizGame.java
@@ -1,18 +1,20 @@
 package _03_print_and_popups._4_quiz_game;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	public static void main(String[] args) {
 		
 		// Create a variable to hold the user's score. Set it equal to zero. 
-		
+		int count= 0;
 		// ASK A QUESTION AND CHECK THE ANSWER
 		
 				// 2.  Ask the user a question 
-				
+		String input = JOptionPane.showInputDialog("What is 100 x 100?");
 				// 3.  Use an if statement to check if their answer is correct
-				
+				if(input.equals(10000)) {
 				// 4.  if the user's answer was correct, add one to their score 
-		
+			 count +=1;
 		// MAKE MORE QUESTIONS. Ask more questions by repeating the above 
 				// Option: Subtract a point from their score for a wrong answer
 		
